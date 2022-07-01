cfaba71fea478ec18548bf9c90787d98e242401f
Kasyap G <kasyap.gundu@testsigma.com>
2022-07-01 14:58:55 +0530

    Removed profileReference for TestDataSetAnnotation

--- src/main/java/com/testsigma/sdk/annotation/TestDataSet.java
+++ src/main/java/com/testsigma/sdk/annotation/TestDataSet.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TestDataSet {
     String reference() default "set-name";
-    String profileReference() default "profile-name";
     String description() default "";
     boolean deprecated() default false;
 }
