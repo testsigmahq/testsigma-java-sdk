3c18eb936a0efd3a5f784ea88bf611c46b2e0b26
Rahul Mohan <rahul.mohan@testsigma.com>
2022-12-27 09:10:05 +0530

    [3.4.2][TE-6507]: Addon updates

--- src/main/java/com/testsigma/sdk/Hook.java
+++ src/main/java/com/testsigma/sdk/Hook.java
@@ -1,7 +1,24 @@
 package com.testsigma.sdk;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 public abstract class Hook {
+    private static final int MESSAGE_MAX_SIZE = 500;
     protected com.testsigma.sdk.Logger logger = new Logger(new StringBuilder(""));
+    protected String errorMessage;
+    protected String successMessage;
+    private StringBuffer loggedMessages = new StringBuffer();
     protected abstract Result execute();
+
+    protected void setErrorMessage(String errorMessage) {
+        this.errorMessage = StringUtils.abbreviate(errorMessage, MESSAGE_MAX_SIZE);
+    }
+
+    protected void setSuccessMessage(String successMessage) {
+        this.successMessage = StringUtils.abbreviate(successMessage, MESSAGE_MAX_SIZE);
+    }
+    protected void log(Object message){
+        loggedMessages.append(message.toString()).append("<br>");
+    }
 }
