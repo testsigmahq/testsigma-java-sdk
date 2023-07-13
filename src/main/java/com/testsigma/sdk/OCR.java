fd9d8dc2777d04d1c8ea19dfabb95c5e7e9a6ad8
Srivalli <navyana.binkam@testsigma.com>
2023-07-13 15:44:04 +0530

    [TE-8388]Image Search Capability for Addon

--- src/main/java/com/testsigma/sdk/OCR.java
+++ src/main/java/com/testsigma/sdk/OCR.java
@@ -1,5 +1,6 @@
 package com.testsigma.sdk;
 
+import java.io.File;
 import java.util.List;
 
 public interface OCR {
@@ -8,4 +9,13 @@ public interface OCR {
     public List<OCRTextPoint> extractTextFromImage(OCRImage image);
 
     public List<OCRTextPoint> extractTextFromElement(Element element);
+
+    public FindImageResponse findImage(String imageURl, Float threshold);
+
+    public FindImageResponse findImage(String imageUrl);
+
+    public  FindImageResponse findImage(String imageUrl, Float threshold, Integer scale);
+
+    public void uploadFile(String s3SignedURL, File baseImageFile);
+
 }
