de8d29d351f93b8bb5998ab12fdcdc9ad735d246
Kasyap <kasyap.gundu@testsigma.com>
2023-08-07 15:21:03 +0530

    Fix: TE-11472 Sending Email from Addon is not working

--- src/main/java/com/testsigma/sdk/Email.java
+++ src/main/java/com/testsigma/sdk/Email.java
@@ -1,5 +1,7 @@
 package com.testsigma.sdk;
 
+import org.apache.http.entity.ContentType;
+
 import java.io.File;
 import java.util.List;
 
@@ -10,6 +12,8 @@ public interface Email {
   void setSubject(String subject);
   void setBody(String body);
   void setAttachments(List<File> attachments);
+
+  void setContentType(ContentType contentType);
 
   Boolean send();
 }
