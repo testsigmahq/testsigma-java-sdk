506a0e9af92c0938614c693c3b6c0fff6c2b865c
Sunil Gembali <gembali.sunil@testsigma.com>
2024-05-16 18:40:29 +0530

    Feat[TE-18438]: Changed the name Rest to RestApi

--- src/main/java/com/testsigma/sdk/RestAction.java
+++
@@ -1,4 +0,0 @@
-package com.testsigma.sdk;
-
-public abstract class RestAction extends DriverAction{
-}
---
+++ src/main/java/com/testsigma/sdk/RestApiAction.java
@@ -0,0 +1,4 @@
+package com.testsigma.sdk;
+
+public abstract class RestApiAction extends Action {
+}
