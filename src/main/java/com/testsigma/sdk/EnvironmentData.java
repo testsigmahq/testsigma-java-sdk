dd4798bc49e1ffb57aa3c02085953c3f17024210
udayareddyvempalli <145988346+udayareddyvempalli@users.noreply.github.com>
2024-07-12 19:21:24 +0530

    [TE-17076]: updated EnvironmentData parameters with List of VariableData (#26)
    (cherry picked from commit 711de1a48c88c3a0d682885f0239cf3c097b30fb)

--- src/main/java/com/testsigma/sdk/EnvironmentData.java
+++ src/main/java/com/testsigma/sdk/EnvironmentData.java
@@ -2,16 +2,21 @@ package com.testsigma.sdk;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Data
 @RequiredArgsConstructor
 public class EnvironmentData {
 
-    private final JSONObject parameters;
+    private final List<VariableData> variables;
 
     public String getParameter(String key){
-        return parameters.get(key).toString();
+        Optional<VariableData> variableData = variables.stream().filter(variable -> variable.getKey().equals(key))
+                .collect(Collectors.toList()).stream().findFirst();
+        return variableData.isPresent() ? variableData.get().getValue() : "";
     }
 
 }
